07ffb1bac3d5cf296906efbc9286836a7d484de4
Tim Whitaker <tmwhtkr@gmail.com>
2018-08-04 18:40:00 -0600

    Scaffold out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,57 @@
 class UsersController < ApplicationController
+  # GET /users
   def index
+    @users = User.all
   end
 
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
+  # GET /users/1
   def show
+    @user = User.find(params[:id])
   end
 
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'Welcome!'
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /users/1
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = 'User updated.'
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
+  # DELETE /users/1
   def destroy
+    @user = user.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
