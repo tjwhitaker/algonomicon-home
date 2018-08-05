dd93ae00e4e0288f90f8dfa542e1e325223329b0
Tim Whitaker <tmwhtkr@gmail.com>
2018-08-04 18:50:27 -0600

    Scaffold out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,24 @@
 class SessionsController < ApplicationController
-  def index
-  end
-
-  def create
-  end
-
+  # GET /login
   def new
+    @user = User.new
   end
 
-  def edit
+  # POST /login
+  def create
+    if @user = login(params[:email], params[:password])
+      flash[:notice] = 'Login successful.'
+      redirect_back_or_to :root_path
+    else
+      flash[:notice] = 'Login failed.'
+      render :new
+    end
   end
 
-  def show
-  end
-
-  def update
-  end
-
+  # DELETE /logout
   def destroy
+    logout
+    flash[:notice] = 'Logged out.'
+    redirect_to :users
   end
 end
