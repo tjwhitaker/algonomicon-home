fb4254855dd58ab232bfdd6832e8b797e126c4d5
Tim Whitaker <tmwhtkr@gmail.com>
2018-08-19 15:14:22 -0600

    Add articles migration

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :articles,   only: [:index, :show]
   resources :categories, only: [:index, :show]
   resources :projects,   only: [:index, :show]
 
   namespace :admin do
     root 'pages#dashboard'
 
+    resources :articles
     resources :categories
     resources :projects
     resources :users
