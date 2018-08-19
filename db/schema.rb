3caebd18d9718462dfecdcdf6cb219b7ec90f567
Tim Whitaker <tmwhtkr@gmail.com>
2018-08-19 15:47:04 -0600

    Add admin articles content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_054934) do
+ActiveRecord::Schema.define(version: 2018_08_19_211330) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "content"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ categories.each do |title|
   Category.create(title: title)
 end
 
-for i in 0..16 do
+for i in 0..8 do
   data = {
     title: [Faker::Lovecraft.deity, Faker::Lovecraft.tome, Faker::Lovecraft.location].sample,              
     description: Faker::Lovecraft.sentence,
@@ -12,6 +12,16 @@ for i in 0..16 do
   }
 
   Project.create(data)
+end
+
+for i in 0..16 do
+  data = {
+    title: [Faker::Lovecraft.deity, Faker::Lovecraft.tome, Faker::Lovecraft.location].sample,              
+    description: Faker::Lovecraft.sentence,
+    content: Faker::Markdown.sandwich
+  }
+
+  Article.create(data)
 end
 
 User.create(email: 'test@algonomicon.com', password: 'test')
