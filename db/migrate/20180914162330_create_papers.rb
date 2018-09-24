d0f36f87e6be841b15246d188286ee7f4b39519e
Tim Whitaker <tmwhtkr@gmail.com>
2018-09-24 15:00:46 -0600

    Adjust typography, about copy, and results

--- db/migrate/20180914162330_create_papers.rb
+++ db/migrate/20180914162330_create_papers.rb
@@ -5,7 +5,7 @@ class CreatePapers < ActiveRecord::Migration[5.2]
 
       t.string :title
       t.string :source
-      t.string :content
+      t.string :implementation
       t.string :authors
       t.text :abstract
       t.date :publish_date
