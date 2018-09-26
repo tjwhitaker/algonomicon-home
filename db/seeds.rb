1e7e56cee72616cfe0c840676230e9ff35a1e414
Tim Whitaker <tmwhtkr@gmail.com>
2018-09-26 14:18:25 -0600

    Remove seeds

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-categories = ['Language', 'Reinforcement', 'Vision']
-
-categories.each do |title|
-  Category.create(title: title)
-end
-
-User.create(email: 'test@algonomicon.com', password: 'test')
