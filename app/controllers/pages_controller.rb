61f0d92025fe3c348d9ba06dbc5b8f2ba7cec067
Tim Whitaker <tmwhtkr@gmail.com>
2018-09-26 12:12:55 -0600

    Add better title logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
 class PagesController < ApplicationController
   def about
-    @title = 'About'
   end
 
   def contact
-    @title = 'Contact'
   end
 
   def home 
-    @title = 'Home'
     @categories = Category.all.order(:title)
     @events = Event.all
     @papers = Paper.order(created_at: :desc).page(params[:page])
