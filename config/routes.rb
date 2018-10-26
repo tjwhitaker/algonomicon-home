891c5832a594d4529d1910c65f4109d933b0f4d4
Tim Whitaker <tmwhtkr@gmail.com>
2018-10-26 14:50:00 -0600

    Start adding datasets and projects

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,20 @@ Rails.application.routes.draw do
 
   resources :articles,   only: [:index, :show]
   resources :categories, only: [:index, :show]
+  resources :datasets,   only: [:index, :show]
   resources :events,     only: [:index, :show]
   resources :papers,     only: [:index, :show]
+  resources :projects,   only: [:index, :show]
 
   namespace :admin do
     root 'pages#dashboard'
 
     resources :articles
     resources :categories
+    resources :datasets
     resources :events
     resources :papers
+    resources :projects
     resources :users
   end
 end
---
+++ db/migrate/20181026204511_create_datasets.rb
@@ -0,0 +1,6 @@
+class CreateDatasets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :datasets do |t|
+    end
+  end
+end
