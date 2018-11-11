48ade20d193f3b69fb717cbaeb0394b700126e06
Tim Whitaker <tmwhtkr@gmail.com>
2018-11-11 13:00:08 -0700

    Add footer styles

--- universe/frontend/src/Layouts/Footer/FooterContainer.js
+++ universe/frontend/src/Layouts/Footer/FooterContainer.js
@@ -1,15 +1,33 @@
 import { Component } from 'inferno'
+import { createComponent } from 'inferno-fela'
+
+const styles = {
+  container: () => ({
+    background: '#111',
+    color: '#ccc'
+  }),
+  wrapper: () => ({
+    maxWidth: '1024px',
+    margin: '0 auto',
+    padding: '1.4rem'
+  }),
+  copyright: () => ({
+    margin: '0'
+  })
+}
+
+const Container = createComponent(styles.container)
+const Wrapper = createComponent(styles.wrapper)
+const Copyright = createComponent(styles.copyright, 'p')
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer-container">
-        <div className="wrapper">
-          <div className="copyright">
-            <p>&copy; Algonomicon LLC 2018</p>
-          </div>
-        </div>
-      </footer>
+      <Container>
+        <Wrapper>
+          <Copyright>&copy; Algonomicon LLC 2018</Copyright>
+        </Wrapper>
+      </Container>
     )
   }
 }
