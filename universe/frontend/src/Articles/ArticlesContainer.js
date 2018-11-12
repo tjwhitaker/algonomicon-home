699b2be97b0df75c5ab467b8fff57b0465f68809
Tim Whitaker <tmwhtkr@gmail.com>
2018-11-12 13:03:13 -0700

    Add articles page styles

--- universe/frontend/src/Articles/ArticlesContainer.js
+++ universe/frontend/src/Articles/ArticlesContainer.js
@@ -1,25 +1,72 @@
 import { Component } from 'inferno'
+import { createComponent } from 'inferno-fela'
 import CategoriesContainer from '../Components/Categories/CategoriesContainer'
 import FiltersContainer from './Filters/FiltersContainer'
 import ShowcaseContainer from './Showcase/ShowcaseContainer'
 
+const styles = {
+  wrapper: () => ({
+    maxWidth: '1024px',
+    margin: '0 auto',
+    padding: '1.4rem'
+  }),
+  grid: () => ({
+    display: 'grid',
+    gridTemplateColumns: 'minmax(0, 2fr) minmax(0, 1fr)',
+    gridTemplateAreas: 
+      `'nav nav'
+       'main sidebar'`
+  }),
+  nav: () => ({
+    gridArea: 'nav'
+  }),
+  main: () => ({
+    gridArea: 'main',
+    borderRight: '1px solid #ccc',
+    paddingRight: '1.4rem'
+  }),
+  sidebar: () => ({
+    gridArea: 'sidebar',
+    paddingLeft: '1.4rem',
+    maxHeight: '100%',
+    overflow: 'hidden'
+  }),
+  heading: () => ({
+    color: '#aaa',
+    borderBottom: '1px solid #ccc',
+    fontSize: '1.4rem',
+    padding: '1.4rem 0',
+    lineHeight: '1',
+    fontFamily: 'monospace',
+    display: 'block',
+    textTransform: 'uppercase'
+  })
+}
+
+const Wrapper = createComponent(styles.wrapper)
+const Grid = createComponent(styles.grid)
+const Nav = createComponent(styles.nav)
+const Main = createComponent(styles.main)
+const Sidebar = createComponent(styles.sidebar)
+const Heading = createComponent(styles.heading, 'h3')
+
 class ArticlesContainer extends Component {
   render() {
     return (
-      <div className="articles-container">
-        <CategoriesContainer />
-        <div className="wrapper">
-          <div className="grid">
-            <div className="primary">
-              <ShowcaseContainer />
-            </div>
-            <div className="secondary">
-              <h3 class="heading">Filters</h3>
-              <FiltersContainer />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Wrapper>
+        <Grid>
+          <Nav>
+            <CategoriesContainer />
+          </Nav>
+          <Main>
+            <ShowcaseContainer />
+          </Main>
+          <Sidebar>
+            <Heading>Filters</Heading>
+            <FiltersContainer />
+          </Sidebar>
+        </Grid>
+      </Wrapper>
     )
   }
 }
