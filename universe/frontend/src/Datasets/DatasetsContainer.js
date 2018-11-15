65b2709fc657377ce6620e1e555a57500cf73e04
Tim Whitaker <tmwhtkr@gmail.com>
2018-11-15 11:10:48 -0700

    Add table container to datasets

--- universe/frontend/src/Datasets/DatasetsContainer.js
+++ universe/frontend/src/Datasets/DatasetsContainer.js
@@ -2,7 +2,7 @@ import { Component } from 'inferno'
 import { createComponent } from 'inferno-fela'
 import CategoriesContainer from '../Components/Categories/CategoriesContainer'
 import FiltersContainer from './Filters/FiltersContainer'
-import ShowcaseContainer from './Showcase/ShowcaseContainer'
+import TableContainer from './Table/TableContainer'
 
 const styles = {
   wrapper: () => ({
@@ -12,10 +12,9 @@ const styles = {
   }),
   grid: () => ({
     display: 'grid',
-    gridTemplateColumns: 'minmax(0, 2fr) minmax(0, 1fr)',
     gridTemplateAreas: 
-      `'nav nav'
-       'main sidebar'`
+      `'nav'
+       'main'`
   }),
   nav: () => ({
     gridArea: 'nav'
@@ -24,22 +23,6 @@ const styles = {
     gridArea: 'main',
     borderRight: '1px solid #ccc',
     paddingRight: '1.4rem'
-  }),
-  sidebar: () => ({
-    gridArea: 'sidebar',
-    paddingLeft: '1.4rem',
-    maxHeight: '100%',
-    overflow: 'hidden'
-  }),
-  heading: () => ({
-    color: '#aaa',
-    borderBottom: '1px solid #ccc',
-    fontSize: '1.4rem',
-    padding: '1.4rem 0',
-    lineHeight: '1',
-    fontFamily: 'monospace',
-    display: 'block',
-    textTransform: 'uppercase'
   })
 }
 
@@ -47,8 +30,6 @@ const Wrapper = createComponent(styles.wrapper)
 const Grid = createComponent(styles.grid)
 const Nav = createComponent(styles.nav)
 const Main = createComponent(styles.main)
-const Sidebar = createComponent(styles.sidebar)
-const Heading = createComponent(styles.heading, 'h3')
 
 class DatasetsContainer extends Component {
   render() {
@@ -59,12 +40,8 @@ class DatasetsContainer extends Component {
             <CategoriesContainer />
           </Nav>
           <Main>
-            <ShowcaseContainer />
+            <TableContainer />
           </Main>
-          <Sidebar>
-            <Heading>Filters</Heading>
-            <FiltersContainer />
-          </Sidebar>
         </Grid>
       </Wrapper>
     )
