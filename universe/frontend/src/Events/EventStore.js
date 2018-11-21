b21234be1bd5e2387252f6831c6e2581123f6e7a
Tim Whitaker <tmwhtkr@gmail.com>
2018-11-20 20:23:21 -0700

    Fetch events from server

--- universe/frontend/src/Events/EventStore.js
+++ universe/frontend/src/Events/EventStore.js
@@ -3,35 +3,10 @@ import { action, observable } from 'mobx'
 class EventStore {
   @observable events = []
 
-  @action
-  fetchEvents() {
-    this.events = [
-      {
-        name: 'NIPS: Neural Information Processing Systems',
-        location: 'Montreal, Canada',
-        date: 'Dec 02 - Dec 08'
-      },
-      {
-        name: 'SC: The International Conference for High Performance Computing',
-        location: 'Dallas, TX',
-        date: 'Aug 19 - Aug 23'
-      },
-      {
-        name: 'ICML: International Conference on Machine Learning',
-        location: 'Stockholm, Sweden',
-        date: 'Jul 10 - Jul 15'
-      },
-      {
-        name: 'CVPR: Computer Vision and Pattern Recognition',
-        location: 'Long Beach, CA',
-        date: 'Jun 15 - Jun 21'
-      },
-      {
-        name: 'KDD: Knowledge Discovery and Data Mining',
-        location: 'London, UK',
-        date: 'Aug 19, Aug 23'
-      }
-    ]
+  @action fetchEvents() {
+    fetch('http://localhost:8000/events')
+      .then(response => response.json())
+      .then(events => this.events = events)
   }
 }
 
