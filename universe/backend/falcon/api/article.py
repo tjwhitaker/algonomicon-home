270b64676c0e345b17d2a4e5ea10248934f499ae
Tim Whitaker <tmwhtkr@gmail.com>
2018-11-25 09:36:06 -0700

    Add crud routes to articles and write test curl

--- universe/backend/falcon/api/article.py
+++ universe/backend/falcon/api/article.py
@@ -1,28 +1,60 @@
 import json
 from db import Base
+from marshmallow import Schema, fields
 from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.sqltypes import Integer, String, Text
 
 class Article(Base):
     __tablename__ = 'article'
     id = Column(Integer, primary_key=True)
     title = Column(String)
+    description = Column(Text)
+    hero = Column(String)
+
+class ArticleSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    hero = fields.Str()
 
 class ArticleResource(object):
     def on_get(self, req, resp, id):
-        item = Article(title='hello world') 
+        article = self.db.query(Article).get(id)
+        data, errors = ArticleSchema().dump(article)
+        resp.media = data
 
-        self.db.add(item)
+    def on_put(self, req, resp, id):
+        article = self.db.query(Article).get(id)
+        article.title = req.media.get('title')
+        article.description = req.media.get('description')
+        article.hero = req.media.get('hero')
+
         self.db.commit()
 
-        resp.media = self.db.query(Article).first().title
+        data, errors = ArticleSchema().dump(article)
+        resp.media = data
+
+    def on_delete(self, req, resp, id):
+        self.db.query(Article).get(id).delete()
+        self.db.commit()
+
+        resp.media = {'message': 'Article Deleted'}
         
 class ArticleCollectionResource(object):    
     def on_get(self, req, resp):
-        item = {
-            'preview': 'https://source.unsplash.com/600x300',
-            'title': 'Hello!',
-            'description': 'This is a test description of a fake article post'
-        }
+        articles = self.db.query(Article).all()
+        data, errors = ArticleSchema(many=True).dump(articles)
+        resp.media = data
 
-        resp.media = [item for i in range(10)]
+    def on_post(self, req, resp):
+        article = Article(
+            title=req.media.get('title'),
+            description = req.media.get('description'),
+            hero=req.media.get('hero')
+        ) 
+
+        self.db.add(article)
+        self.db.commit()
+
+        data, errors = ArticleSchema().dump(article)
+        resp.media = data
