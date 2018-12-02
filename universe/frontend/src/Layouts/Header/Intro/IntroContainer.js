d7da11e86dc11335abe70862a6907d88916e9a95
Tim Whitaker <tmwhtkr@gmail.com>
2018-12-02 10:55:10 -0700

    Add input events to email subscription form

--- universe/frontend/src/Layouts/Header/Intro/IntroContainer.js
+++ universe/frontend/src/Layouts/Header/Intro/IntroContainer.js
@@ -1,5 +1,4 @@
 import { Component } from 'inferno'
-import { Link } from 'inferno-router'
 import { createComponent } from 'inferno-fela'
 
 const styles = {
@@ -71,26 +70,41 @@ const styles = {
 const Container = createComponent(styles.container)
 const Tagline = createComponent(styles.tagline, 'h2')
 const Title = createComponent(styles.title, 'h1')
-const SubscribeContainer = createComponent(styles.searchContainer)
+const SubscribeContainer = createComponent(styles.subscribeContainer)
 const InputContainer = createComponent(styles.inputContainer)
 const Icon = createComponent(styles.icon)
-const Input = createComponent(styles.input, 'input', ['type', 'name', 'placeholder'])
-const Button = createComponent(styles.button, 'button')
+const Input = createComponent(styles.input, 'input', ['type', 'value', 'onInput', 'placeholder'])
+const Button = createComponent(styles.button, 'button', ['onclick'])
 
 class IntroContainer extends Component {
-  /*
-    subscribe() {
-      let data = {
-        EMAIL: ''
-      }
-      
-      validate email
-      
-      fetch('https://algonomicon.us12.list-manage.com/subscribe/post-json?u=884c4febd2fb609267a7f7d66&amp;id=c6e91d9210&amp;c=?')
+  constructor(props) {
+    super(props)
+
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    let data = {email: this.state.value}
     
-      show modal success or error`
+    let options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
     }
-  */
+
+    fetch('http://localhost:8000/users', options)
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(error => console.log(error))
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
 
   render() {
     return (
@@ -98,15 +112,13 @@ class IntroContainer extends Component {
         <Title>The Place for Machine Learning</Title>
         <Tagline>Still under construction. Thanks for bearing with us! Sign up for early access.</Tagline>
         <SubscribeContainer>
-          <form action="https://algonomicon.us12.list-manage.com/subscribe/post?u=884c4febd2fb609267a7f7d66&amp;id=c6e91d9210" method="post">
-            <InputContainer>
-              <Icon>
-                <svg style={{display: 'block'}} aria-hidden="true" data-prefix="fas" data-icon="user-astronaut" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-user-astronaut fa-w-14 fa-3x"><path fill="currentColor" d="M64 224h13.5c24.7 56.5 80.9 96 146.5 96s121.8-39.5 146.5-96H384c8.8 0 16-7.2 16-16v-96c0-8.8-7.2-16-16-16h-13.5C345.8 39.5 289.6 0 224 0S102.2 39.5 77.5 96H64c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16zm40-88c0-22.1 21.5-40 48-40h144c26.5 0 48 17.9 48 40v24c0 53-43 96-96 96h-48c-53 0-96-43-96-96v-24zm72 72l12-36 36-12-36-12-12-36-12 36-36 12 36 12 12 36zm151.6 113.4C297.7 340.7 262.2 352 224 352s-73.7-11.3-103.6-30.6C52.9 328.5 0 385 0 454.4v9.6c0 26.5 21.5 48 48 48h80v-64c0-17.7 14.3-32 32-32h128c17.7 0 32 14.3 32 32v64h80c26.5 0 48-21.5 48-48v-9.6c0-69.4-52.9-125.9-120.4-133zM272 448c-8.8 0-16 7.2-16 16s7.2 16 16 16 16-7.2 16-16-7.2-16-16-16zm-96 0c-8.8 0-16 7.2-16 16v48h32v-48c0-8.8-7.2-16-16-16z" class=""></path></svg>
-              </Icon>
-              <Input type="email" name="EMAIL" placeholder="example@algonomicon.com" />
-            </InputContainer>
-            <Button>Sign Up!</Button>
-          </form>
+          <InputContainer>
+            <Icon>
+              <svg style={{display: 'block'}} aria-hidden="true" data-prefix="fas" data-icon="user-astronaut" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-user-astronaut fa-w-14 fa-3x"><path fill="currentColor" d="M64 224h13.5c24.7 56.5 80.9 96 146.5 96s121.8-39.5 146.5-96H384c8.8 0 16-7.2 16-16v-96c0-8.8-7.2-16-16-16h-13.5C345.8 39.5 289.6 0 224 0S102.2 39.5 77.5 96H64c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16zm40-88c0-22.1 21.5-40 48-40h144c26.5 0 48 17.9 48 40v24c0 53-43 96-96 96h-48c-53 0-96-43-96-96v-24zm72 72l12-36 36-12-36-12-12-36-12 36-36 12 36 12 12 36zm151.6 113.4C297.7 340.7 262.2 352 224 352s-73.7-11.3-103.6-30.6C52.9 328.5 0 385 0 454.4v9.6c0 26.5 21.5 48 48 48h80v-64c0-17.7 14.3-32 32-32h128c17.7 0 32 14.3 32 32v64h80c26.5 0 48-21.5 48-48v-9.6c0-69.4-52.9-125.9-120.4-133zM272 448c-8.8 0-16 7.2-16 16s7.2 16 16 16 16-7.2 16-16-7.2-16-16-16zm-96 0c-8.8 0-16 7.2-16 16v48h32v-48c0-8.8-7.2-16-16-16z" class=""></path></svg>
+            </Icon>
+            <Input type="email" value={this.state.value} onInput={this.handleChange} placeholder="example@algonomicon.com" />
+          </InputContainer>
+          <Button onclick={this.handleSubmit}>Sign Up!</Button>
         </SubscribeContainer>
       </Container>
     )
