f0b5f49b05ef99e1ee61d6ed1bc649bb27f5f4dd
Tim Whitaker <tmwhtkr@gmail.com>
2018-12-25 12:11:43 -0700

    Add feed endpoint and sort by created time

--- backend/src/api/feed.py
+++ backend/src/api/feed.py
@@ -1,13 +1,27 @@
 import falcon
-from api.article import Article 
-from api.dataset import Dataset
-from api.event   import Event
-from api.model   import Model
-from api.paper   import Paper
-from api.project import Project
+from api.article import Article, ArticleSchema
+from api.dataset import Dataset, DatasetSchema
+from api.event   import Event, EventSchema
+from api.model   import Model, ModelSchema
+from api.paper   import Paper, PaperSchema
+from api.project import Project, ProjectSchema
 
 class FeedResource:
     def on_get(self, req, resp):
-        items = self.db.query(Article, Dataset, Event, Model, Paper, Project).all()
+        articles, _ = ArticleSchema(many=True).dump(self.db.query(Article).all())
+        datasets, _ = DatasetSchema(many=True).dump(self.db.query(Dataset).all())
+        events, _ = EventSchema(many=True).dump(self.db.query(Event).all())
+        models, _ = ModelSchema(many=True).dump(self.db.query(Model).all())
+        papers, _ = PaperSchema(many=True).dump(self.db.query(Paper).all())
+        projects, _ = ProjectSchema(many=True).dump(self.db.query(Project).all())
 
-        print(items)
+        [article.update({'type':'article'}) for article in articles]
+        [dataset.update({'type':'dataset'}) for dataset in datasets]
+        [event.update({'type':'event'}) for event in events]
+        [model.update({'type':'model'}) for model in models]
+        [paper.update({'type':'paper'}) for paper in papers]
+        [project.update({'type':'project'}) for project in projects]
+
+        items = articles + datasets + events + models + papers + projects
+
+        resp.media = sorted(items, key=lambda x: x['created_at']) 
