0c8bdb516252c3bd30b71a7d4c4dfd7132daf0c2
Tim Whitaker <tmwhtkr@gmail.com>
2019-01-14 14:22:53 -0700

    Add animation to loading text

--- frontend/src/Shared/Loading/LoadingContainer.js
+++ frontend/src/Shared/Loading/LoadingContainer.js
@@ -1,9 +1,28 @@
 import { Component } from 'inferno'
 
 class LoadingContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loadingText: 'Loading'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.loadingText === 'Loading...') { 
+        this.setState({loadingText: 'Loading'})
+      }
+      else {
+        this.setState({loadingText: this.state.loadingText + '.'})
+      }
+    }, 500)
+  }
+
   render() {
     return (
-      <div>Loading...</div>
+      <div>{this.state.loadingText}</div>
     )
   }
 }
