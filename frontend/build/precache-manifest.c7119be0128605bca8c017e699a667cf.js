5208e403f34666bf998a82c774d60de80a4f4132
Tim Whitaker <tmwhtkr@gmail.com>
2019-01-21 16:07:10 -0700

    Add frontend build

---
+++ frontend/build/precache-manifest.c7119be0128605bca8c017e699a667cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3051a5401b3d1c25c0a537f2e7cf3154",
+    "url": "/static/media/space.3051a540.jpg"
+  },
+  {
+    "revision": "3438ab4c4214d826737d074be69de7c7",
+    "url": "/static/media/nebula.3438ab4c.jpg"
+  },
+  {
+    "revision": "43723a1e05560dc9fab4d600c35651c1",
+    "url": "/static/media/inception.43723a1e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7f923df4d0c92760b9a",
+    "url": "/static/js/main.f7f923df.chunk.js"
+  },
+  {
+    "revision": "c5c3a8e50051027a4126",
+    "url": "/static/js/1.c5c3a8e5.chunk.js"
+  },
+  {
+    "revision": "f7f923df4d0c92760b9a",
+    "url": "/static/css/main.c442a08d.chunk.css"
+  },
+  {
+    "revision": "dd904da25ac38d6df520a9747a787b06",
+    "url": "/index.html"
+  }
+];
