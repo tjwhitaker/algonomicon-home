123fff47ed91ccb3b29f943ff44b67a70ba2a766
Tim Whitaker <tmwhtkr@gmail.com>
2019-03-27 11:03:46 -0600

    Remove events from router

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,6 @@
 import compose from 'koa-compose'
 import Articles from './contexts/articles'
 import Datasets from './contexts/datasets'
-import Events from './contexts/events'
 import Papers from './contexts/papers'
 import Projects from './contexts/projects'
 import Videos from './contexts/videos'
@@ -11,7 +10,6 @@ const Router = () => {
   return compose([
     Articles.routes(), 
     Datasets.routes(),
-    Events.routes(),
     Papers.routes(),
     Projects.routes(),
     Videos.routes(),
