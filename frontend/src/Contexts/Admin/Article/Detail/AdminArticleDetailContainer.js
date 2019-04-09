80f7fbd9c72011430cb153927858a32ef109a818
Tim Whitaker <tmwhtkr@gmail.com>
2019-04-09 12:33:57 -0600

    Add article detail fields

--- frontend/src/Contexts/Admin/Article/Detail/AdminArticleDetailContainer.js
+++ frontend/src/Contexts/Admin/Article/Detail/AdminArticleDetailContainer.js
@@ -1,5 +1,51 @@
-const AdminArticleDetailContainer = () => (
-  <div>Article Detail</div>
-)
+import { inject, observer } from 'inferno-mobx'
 
-export default AdminArticleDetailContainer
+const AdminArticleDetailContainer = ({ ArticleStore, match: {params} }) => {
+  const article = ArticleStore.fetchArticle(params.slug)
+
+  return (
+    <div>
+      { article && (
+        <div>
+          <div>
+            <label>Name:</label>
+            <input type="text" value={article.name} />
+          </div>
+
+          <div>
+            <label>Slug:</label>
+            <input type="text" value={article.slug} />
+          </div>
+
+          <div>
+            <label>Author:</label>
+            <input type="text" value={article.author} />
+          </div>
+
+          <div>
+            <label>Hero:</label>
+            <input type="text" value={article.hero} />
+          </div>
+
+          <div>
+            <label>Description:</label> 
+            <textarea rows="5" value={article.description} />
+          </div>
+
+          <div>
+            <label>Outline:</label> 
+            <textarea rows="10" value={article.outline} />
+          </div>
+
+          <div>
+            <label>Content:</label> 
+            <textarea rows="10" value={article.content} />
+          </div>
+
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default inject('ArticleStore')(observer(AdminArticleDetailContainer))
