3e040c652e813b9382c81ccb69eec0296da89d33
Tim Whitaker <tmwhtkr@gmail.com>
2019-04-09 13:15:48 -0600

    Fix overflow on admin main

--- frontend/src/Shared/Layout/Admin/Main/MainContainer.js
+++ frontend/src/Shared/Layout/Admin/Main/MainContainer.js
@@ -3,7 +3,8 @@ import { createComponent } from 'inferno-fela'
 const Main = createComponent(() => ({
   gridArea: 'main',
   background: 'white',
-  padding: '1.5rem'
+  padding: '1.5rem',
+  overflow: 'auto'
 }))
 
 const MainContainer = (props) => (
