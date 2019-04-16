bc12795ffdd5e3ff69194aee486f93ad471bb4e4
Tim Whitaker <tmwhtkr@gmail.com>
2019-04-16 11:09:30 -0600

    Add dataset schema

--- backend/schemas/dataset.js
+++ backend/schemas/dataset.js
@@ -4,7 +4,40 @@ export default {
   type: "document",
   fields: [
     {
-      
+      title: "Title",
+      name: "title",
+      type: "string"
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug"
+    },
+    {
+      title: "Creator",
+      name: "creator",
+      type: "string"
+    },
+    {
+      title: "Format",
+      name: "format",
+      type: "string"
+    },
+    {
+      title: "Instances",
+      name: "instances",
+      type: "number"
+    },
+    {
+      title: "Description",
+      name: "description",
+      type: "text"
+    },
+    {
+      title: "Content",
+      name: "content",
+      type: "array",
+      of: [{type: "block"}]
     }
   ]
 }
--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import article from './article'
+import dataset from './dataset'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +14,7 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    article
+    article,
+    dataset
   ])
 })
