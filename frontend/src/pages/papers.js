eef8e3762fbb4af0ebddaea3a7d34a19036134c7
Tim Whitaker <tmwhtkr@gmail.com>
2019-04-18 09:59:00 -0600

    Add paper index styles

--- frontend/src/pages/papers.js
+++ frontend/src/pages/papers.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from 'styled-components'
 import Layout from '../components/layout/layout'
 import Grid from '../components/layout/grid/grid'
 import Main from '../components/layout/grid/main'
@@ -6,12 +7,39 @@ import Sidebar from '../components/layout/grid/sidebar'
 import Search from '../components/sidebar/search'
 import Sort from '../components/sidebar/sort'
 import Tags from '../components/sidebar/tags'
+import { Link } from 'gatsby'
 
-export default () => (
+const Post = styled.div`
+  padding: 1rem 0;
+  border-bottom:1px solid #ccc;
+  
+  h3 {
+    color: hsla(0, 0%, 0%, 0.8);
+    margin-top:0;
+  }
+
+  p {
+    color: hsla(0, 0%, 0%, 0.8);
+    font-weight: normal;
+  }
+
+  &:first-child {
+    padding-top: 0;
+  }
+`
+
+export default ({ data }) => (
   <Layout>
     <Grid>
       <Main>
-        Papers
+          { data.allSanityPaper.edges.map(({ node }) => 
+            <Post>
+              <Link to={`/papers/${node.slug.current}`}>
+                <h3>{node.title}</h3>
+                <p>{node.abstract.substring(0, 300)}...</p>
+              </Link>
+            </Post>
+          )}
       </Main>
       <Sidebar>
         <Search />
@@ -21,3 +49,20 @@ export default () => (
     </Grid>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    allSanityPaper {
+      edges {
+        node {
+          title,
+          slug {
+            current
+          },
+          author,
+          abstract
+        }
+      }
+    }
+  }
+`
