da5e27e540db9e495e4b2bc05b7e034f52bdb06a
Tim Whitaker <tmwhtkr@gmail.com>
2019-05-03 07:53:55 -0600

    Remove hero image from articles

--- frontend/src/pages/articles.js
+++ frontend/src/pages/articles.js
@@ -7,45 +7,26 @@ import Sidebar from "../components/layout/sidebar"
 import Search from "../components/sidebar/search"
 import Sort from "../components/sidebar/sort"
 import Tags from "../components/sidebar/tags"
-import Image from "gatsby-image"
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
 export default ({ data: {articles} }) => {
-  const [head, ...tail] = articles.edges
-
   return (
     <SidebarLayout>
       <Helmet>
         <title>Articles | Algonomicon</title>
       </Helmet>
       <Main>
-        <Feature>
-          <Link to={`/articles/${head.node.slug.current}`}>
-            <div>
-              <Image fluid={head.node.heroImage.asset.fluid} />
-            </div>
-            <Content>
-              <h3>{head.node.title}</h3>
-              <p>{head.node.description.substring(0, 140)}...</p>
-            </Content>
-          </Link>
-        </Feature>
-        <Grid>
-          {tail.map(({ node }) => (
-            <Article>
-              <Link to={`/articles/${node.slug.current}`}>
-                <div>
-                  <Image fluid={node.heroImage.asset.fluid} />
-                </div>
-                <div>
-                  <h4>{node.title}</h4>
-                  <p>{node.description.substring(0, 140)}...</p>
-                </div>
-              </Link>
-            </Article>
-          ))}
-        </Grid>
+        {articles.edges.map(({ node }) => (
+          <Post>
+            <Link to={`/articles/${node.slug.current}`}>
+              <div>
+                <h3>{node.title}</h3>
+                <p>{node.description.substring(0, 300)}...</p>
+              </div>
+            </Link>
+          </Post>
+        ))}
       </Main>
       <Sidebar>
         <Search />
@@ -70,13 +51,6 @@ export const query = graphql`
           slug {
             current
           }
-          heroImage {
-            asset {
-              fluid {
-                ...GatsbySanityImageFluid
-              }
-            }
-          }
           description
         }
       }
@@ -84,46 +58,21 @@ export const query = graphql`
   }
 `
 
-const Feature = styled.div`
-  flex: 0 0 100%;
-  position: relative;
-`
-
-const Content = styled.div`
-  background: hsla(0, 0%, 0%, 0.8);
-  position:absolute;
-  bottom:1rem;
-  left:1rem;
-  max-width:60%;
-  padding:1rem;
+const Post = styled.div`
+  padding:1rem 0;
+  border-bottom:1px solid #ccc;
 
   h3 {
-    color: #fff;
-    margin-top:0;
-  }
-
-  p {
-    font-weight:normal;
-    color: #eee;
-  }
-`
-
-const Grid = styled.div`
-  display:grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1rem;
-`
-
-const Article = styled.div`
-  margin:1rem 0;
-
-  h4 {
     color:hsla(0,0%,0%,0.8); 
-    margin: 0.5rem 0;
+    margin-top: 0;
   }
 
   p {
     color: hsla(0,0%,0%,0.8);
     font-weight:normal;
+  }
+
+  &:first-child {
+    padding-top:0;
   }
 `
