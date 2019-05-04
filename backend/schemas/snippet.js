47fc458d522f80c6811996584839c85dec5e3e1e
Tim Whitaker <tmwhtkr@gmail.com>
2019-05-04 17:31:53 -0600

    Add snippets

---
+++ backend/schemas/snippet.js
@@ -0,0 +1,30 @@
+export default {
+  title: "Snippet",
+  name: "snippet",
+  type: "document",
+  fields: [
+    {
+      title: "Title",
+      name: "title",
+      type: "string"
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title"
+      }
+    },
+    {
+      title: "Content",
+      name: "content",
+      type: "array",
+      of: [
+        {type: "block"},
+        {type: "code"},
+        {type: "image"}
+      ]
+    }
+  ]
+}
