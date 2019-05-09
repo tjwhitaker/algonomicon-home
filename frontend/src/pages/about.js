6c48b1f40412b98b950e78f6f12f009db659ba81
Tim Whitaker <tmwhtkr@gmail.com>
2019-05-09 12:09:54 -0600

    Update about content

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -30,6 +30,8 @@ export default ({data}) => (
       <p>I'm going to be pursuing a M.S. in Computer Science in the fall at Colorado State University. I'm really excited to do research in machine learning and in the mean time, I'm writing articles, building software and collecting datasets in order to learn as much as I can.</p>
 
       <p>I'm hoping to turn Algonomicon into a platform for kickstarting amazing machine learning projects. I want to make it easier for people to work on problems that can have a positive and lasting impact on the world.</p>
+
+      <p>If you have ideas for making Algonomicon even better, I'd love to hear them! Please feel free to reach out to me at tim@algonomicon.com.</p>
     </Main>
     <Sidebar>
       <div>
