7856867359f6f85158a1b27e2a16834b51024603
Tim Whitaker <camp4climber@gmail.com>
2019-06-01 09:30:14 -0600

    Remove unused import

--- frontend/src/templates/algorithm.js
+++ frontend/src/templates/algorithm.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import BlockContent from "@sanity/block-content-to-react"
 import serializers from "../utils/serializers"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 import { Layout, Main, Sidebar, Minion } from "../components"
 
