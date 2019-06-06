68f2ad447433b34c0bcf04521b496e6254137177
Tim Whitaker <camp4climber@gmail.com>
2019-06-06 11:07:12 -0600

    Update typography to be typescript compatible

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,13 +1,48 @@
 import Typography from "typography"
-import githubTheme from "typography-theme-github"
 
-githubTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-  "h1, h2": {
-    borderBottom: 0,
-  },
+// import githubTheme from "typography-theme-github"
+
+// githubTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+//   "h1, h2": {
+//     borderBottom: 0,
+//   },
+// })
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.625,
+  headerFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+  ],
+  bodyFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+  ],
+  scaleRatio: 2,
+  bodyColor: "hsla(0,0%,0%,0.8)",
+  headerWeight: 600,
+  bodyWeight: "normal",
+  boldWeight: 600,
+  // Github has all block elements use 1/2 rhythm not a full rhythm.
+  blockMarginBottom: 1 / 2,
 })
-
-const typography = new Typography(githubTheme)
 
 // Export helper functions
 export const { scale, rhythm, options } = typography
