5182c16e5c9177689222c211516d7ab4ceb46c94
Tim Whitaker <camp4climber@gmail.com>
2019-06-06 12:24:12 -0600

    Convert typography util to typescript

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,13 +1,5 @@
 import Typography from "typography"
 
-// import githubTheme from "typography-theme-github"
-
-// githubTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-//   "h1, h2": {
-//     borderBottom: 0,
-//   },
-// })
-
 const typography = new Typography({
   baseFontSize: "16px",
   baseLineHeight: 1.625,
@@ -40,10 +32,8 @@ const typography = new Typography({
   headerWeight: 600,
   bodyWeight: "normal",
   boldWeight: 600,
-  // Github has all block elements use 1/2 rhythm not a full rhythm.
   blockMarginBottom: 1 / 2,
 })
 
-// Export helper functions
 export const { scale, rhythm, options } = typography
 export default typography
