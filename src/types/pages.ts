b7423b4f5c3778b6518026475e6b531c481a6019
Tim Whitaker <camp4climber@gmail.com>
2019-06-08 18:48:36 -0600

    Add gatsby-image types

---
+++ src/types/pages.ts
@@ -0,0 +1,17 @@
+import { ImageProps } from "./images"
+
+export type IndexProps = {
+  data: {
+    file: {
+      childImageSharp: ImageProps
+    }
+  }
+}
+
+export type AboutProps = {
+  data: {
+    file: {
+      childImageSharp: ImageProps
+    }
+  }
+}
