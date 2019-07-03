13632c0f2d619a3f5b696fb39c5f279c49fb10bb
Tim Whitaker <camp4climber@gmail.com>
2019-07-02 23:22:49 -0600

    Add simple load more state to papers

--- src/pages/papers.tsx
+++ src/pages/papers.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { Link, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
@@ -22,26 +22,39 @@ export const query = graphql`
     }
   }
 `
-export default ({ data }: PapersProps) => (
-  <Layout>
-    <Helmet>
-      <title>Papers | Algonomicon</title>
-    </Helmet>
-    <Main>
-      {data.papers.nodes.map((node, i) => (
-        <Post key={i}>
-          <Link to={`/papers/${node.frontmatter.slug}`}>
-            <h3>{node.frontmatter.title}</h3>
-            <p>{node.excerpt}</p>
-            <small>{node.frontmatter.authors}</small>
-          </Link>
-        </Post>
-      ))}
-    </Main>
-    <Sidebar>
-      <Search />
-      <Sort />
-      <Tags />
-    </Sidebar>
-  </Layout>
-)
+export default ({ data }: PapersProps) => {
+  const [papers, setPapers] = useState(data.papers.nodes.slice(0, 3))
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Papers | Algonomicon</title>
+      </Helmet>
+      <Main>
+        {papers.map((node, i) => (
+          <Post key={i}>
+            <Link to={`/papers/${node.frontmatter.slug}`}>
+              <h3>{node.frontmatter.title}</h3>
+              <p>{node.excerpt}</p>
+              <small>{node.frontmatter.authors}</small>
+            </Link>
+          </Post>
+        ))}
+        <Button onClick={() => setPapers(data.papers.nodes)}>
+          Load more...
+        </Button>
+      </Main>
+      <Sidebar>
+        <Search />
+        <Sort />
+        <Tags />
+      </Sidebar>
+    </Layout>
+  )
+}
+
+const Button = styled.a`
+  display: block;
+  margin: 1rem 0;
+  cursor: pointer;
+`
