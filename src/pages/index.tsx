ccb2bdfaa1156cce6f30ca1156bbe7bdfd05aa21
Tim Whitaker <camp4climber@gmail.com>
2019-07-06 20:07:31 -0600

    Improve paper chunking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,32 +26,35 @@ export default ({ data }: IndexProps) => (
     </Helmet>
     <Main>
       <p>
+        Welcome to Algonomicon! We are a data company working to apply machine
+        learning algorithms to the natural sciences. We're analyzing data,
+        building models and writing tutorials *to showcase how powerful these
+        methods can be*.
+      </p>
+      <p>
         Thanks to increased power and large amounts of data, machine learning
         models are capable of solving problems previously thought to be
-        impossible. Machine Learning applications are driving cars, improving
-        healthcare diagnosis, and sequencing new proteins. Machine learning has
-        the power to do a lot of good and we want to tell the stories of people
-        using it to change the world.
+        impossible.
       </p>
-
       <Image fluid={data.file.childImageSharp.fluid} alt="Inception" />
-
       <p style={{ margin: "1rem 0" }}>
         <i>
           Inception: A state of the art deep learning architecture developed by
           the AI research team at Google.
         </i>
       </p>
-
       <blockquote>
         Truth is much too complicated to allow anything but approximations.
         <p>
           <i>- John Von Neumann</i>
         </p>
       </blockquote>
-
+      <p>
+        We are collecting more data about our universe than ever! We are
+        observing galaxies billions of light years away and particles billions
+        of times smaller than an atom.
+      </p>
       <p>We're particularly interested in the following research topics:</p>
-
       <ul>
         <li>Climate Change and Anthropogenic Impacts on the Environment</li>
         <li>Water Scarcity and Pollution</li>
@@ -61,14 +64,12 @@ export default ({ data }: IndexProps) => (
         <li>Wildlife Conservation and Biodiversity Loss</li>
         <li>Renewable and Alternative Energy</li>
       </ul>
-
       <p>
         If you own or work at a company that is tackling global or social
         issues, we'd love to get in touch. Feel free to visit our{" "}
         <a href="/contact">contact page</a> or reach out directly to
         info@algonomicon.com.
       </p>
-
       <p>
         We support open source software. If you like our content, please join
         our mission and collaborate with us on{" "}
