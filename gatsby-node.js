fc9ce3c799f0538b539a5018ca5467b99f32efb3
Tim Whitaker <camp4climber@gmail.com>
2019-09-02 12:59:07 -0600

    Generate content to /blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,59 +7,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const result = await graphql(`
     {
-      algorithms: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "algorithms" } } }
-      ) {
-        nodes {
-          id
-          frontmatter {
-            slug
-          }
-        }
-      }
-      datasets: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "datasets" } } }
-      ) {
-        nodes {
-          id
-          frontmatter {
-            slug
-          }
-        }
-      }
-      models: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "models" } } }
-      ) {
-        nodes {
-          id
-          frontmatter {
-            slug
-          }
-        }
-      }
-      papers: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "papers" } } }
-      ) {
-        nodes {
-          id
-          frontmatter {
-            slug
-          }
-        }
-      }
-      snippets: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "snippets" } } }
-      ) {
-        nodes {
-          id
-          frontmatter {
-            slug
-          }
-        }
-      }
-      thoughts: allMarkdownRemark(
-        filter: { fields: { collection: { eq: "thoughts" } } }
-      ) {
+      allMarkdownRemark {
         nodes {
           id
           frontmatter {
@@ -70,69 +18,14 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  const algorithms = result.data.algorithms.nodes || []
-  const datasets = result.data.datasets.nodes || []
-  const models = result.data.models.nodes || []
-  const papers = result.data.papers.nodes || []
-  const snippets = result.data.snippets.nodes || []
-  const thoughts = result.data.thoughts.nodes || []
+  const posts = result.data.allMarkdownRemark.nodes || []
 
-  algorithms.forEach(node => {
-    const path = `/algorithms/${node.frontmatter.slug}`
+  posts.forEach(node => {
+    const path = `/blog/${node.frontmatter.slug}`
 
     createPage({
       path,
       component: require.resolve("./src/templates/algorithm.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-
-  datasets.forEach(node => {
-    const path = `/datasets/${node.frontmatter.slug}`
-
-    createPage({
-      path,
-      component: require.resolve("./src/templates/dataset.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-
-  models.forEach(node => {
-    const path = `/models/${node.frontmatter.slug}`
-
-    createPage({
-      path,
-      component: require.resolve("./src/templates/model.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-
-  papers.forEach(node => {
-    const path = `/papers/${node.frontmatter.slug}`
-
-    createPage({
-      path,
-      component: require.resolve("./src/templates/paper.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-
-  snippets.forEach(node => {
-    const path = `/snippets/${node.frontmatter.slug}`
-
-    createPage({
-      path,
-      component: require.resolve("./src/templates/snippet.js"),
-      context: { slug: node.frontmatter.slug },
-    })
-  })
-
-  thoughts.forEach(node => {
-    const path = `/thoughts/${node.frontmatter.slug}`
-
-    createPage({
-      path,
-      component: require.resolve("./src/templates/thought.js"),
       context: { slug: node.frontmatter.slug },
     })
   })
